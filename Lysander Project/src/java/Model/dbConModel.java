f4acedf1905617a4b992a396caff77ea92b50d2b
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2021-12-28 13:58:48 +0530

    connection string set

--- Lysander Project/src/java/Model/dbConModel.java
+++ Lysander Project/src/java/Model/dbConModel.java
@@ -4,11 +4,18 @@
  * and open the template in the editor.
  */
 package Model;
-
+import java.sql.*;
 /**
  *
  * @author Sandaru Jayathilaka
  */
 public class dbConModel {
+    
+    public Connection createConnection() throws ClassNotFoundException, SQLException
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+                return con;
+    }
     
 }
