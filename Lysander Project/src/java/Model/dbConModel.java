26068bb1b2867c113e95d1b823c822e04221a8ae
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2022-01-04 19:30:48 +0530

    db main check 1

--- Lysander Project/src/java/Model/dbConModel.java
+++ Lysander Project/src/java/Model/dbConModel.java
@@ -7,7 +7,7 @@ public class dbConModel {
 
     public Connection createConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotellysander", "root", "");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
         return con;
     }
 
@@ -28,4 +28,21 @@ public class dbConModel {
         return st;
 
     }
+    public  boolean regUser(String fName, String lName, String email,String country,String Nic,String Pno) throws ClassNotFoundException, SQLException 
+                {
+                    PreparedStatement ps = createConnection().prepareStatement("insert into guest values(?,?,?,?,?,?)");
+                    ps.setString(1, fName);
+                    ps.setString(2, lName);
+                    ps.setString(3, email);
+                    ps.setString(4, country);
+                    ps.setString(5, Nic);
+                    ps.setString(6, Pno);
+                    int i = ps.executeUpdate();
+
+                    if(i > 0) 
+                        return true;
+                      else
+                        return false;
+                    
+                }
 }
