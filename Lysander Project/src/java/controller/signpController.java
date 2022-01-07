b6baf746c9a54178bdf02d5c34d3156cc799594c
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2022-01-07 11:44:48 +0530

    edit

--- Lysander Project/src/java/controller/signpController.java
+++ Lysander Project/src/java/controller/signpController.java
@@ -58,6 +58,7 @@ public class signpController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         processRequest(request, response);
     }
 
